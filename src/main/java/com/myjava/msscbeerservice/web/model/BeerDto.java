3ba85354340b8e586283b27f622a456828379a0b
Tommi <tommi.korteikko@iCloud.com>
2020-07-20 11:51:17 +0300

    Added validation to beer DTO

--- src/main/java/com/myjava/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/myjava/msscbeerservice/web/model/BeerDto.java
@@ -4,6 +4,12 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.NotNull;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,19 +21,28 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDto {
 
+	@Null
 	private UUID id;
+	@Null
 	private Integer version;
-	
+	@Null
 	private OffsetDateTime createdDate;
+	@Null
 	private OffsetDateTime lastmodifiedDate;
 	
+	@NotBlank
 	private String beerName;
-	
+	@NotNull
 	private BeerStyleEnum beerStyle;
 	
+	@Positive
+	@NotNull
 	private Long upc;
 	
+	@Positive
+	@NotNull
 	private BigDecimal price;
+	
 	
 	private Integer qtyOnHand;
 	
