929c7bcc0b8ab0655c9f4dec31493aacb52122bc
Tommi <tommi.korteikko@iCloud.com>
2020-07-20 21:32:35 +0300

    Constraint violation exception handler added

--- src/main/java/com/myjava/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/myjava/msscbeerservice/web/controller/BeerController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,13 +26,13 @@ public class BeerController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto){
+	public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beerDto){
 		//implement me
 		return new ResponseEntity<BeerDto>(HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/{beerId}")
-	public ResponseEntity<BeerDto>updateBeerById(@PathVariable("beerId")UUID beerId){
+	public ResponseEntity<BeerDto>updateBeerById(@Validated @PathVariable("beerId")UUID beerId){
 		//implement me 
 		return new ResponseEntity<BeerDto>(HttpStatus.NO_CONTENT);
 	}
