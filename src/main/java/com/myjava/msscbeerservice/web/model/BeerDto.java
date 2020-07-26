58a8a56ff57a457c7547b75b953fdecfd966aab9
Tommi <tommi.korteikko@iCloud.com>
2020-07-26 18:31:01 +0300

    Adding Jackson formater for beerDto

--- src/main/java/com/myjava/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/myjava/msscbeerservice/web/model/BeerDto.java
@@ -5,10 +5,11 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,9 +26,13 @@ public class BeerDto {
 	private UUID id;
 	@Null
 	private Integer version;
+	
 	@Null
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss2",shape=JsonFormat.Shape.STRING)
 	private OffsetDateTime createdDate;
+	
 	@Null
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss2",shape=JsonFormat.Shape.STRING)
 	private OffsetDateTime lastmodifiedDate;
 	
 	@NotBlank
@@ -39,6 +44,7 @@ public class BeerDto {
 	@NotNull
 	private Long upc;
 	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	@Positive
 	@NotNull
 	private BigDecimal price;
